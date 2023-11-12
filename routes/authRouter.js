57fc1868a7313ae3d71c0e82e3dde1c9d2730472
yigitbaris <bariskaynakoglu01@gmail.com>
2023-11-12 23:41:24 +0300

    authenticate function added

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,8 +2,12 @@ import { Router } from 'express'
 const router = Router()
 
 import { register, login } from '../controllers/authController.js'
+import {
+  validateLoginInput,
+  validateRegisterInput,
+} from '../middleware/validationMiddleware.js'
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', validateRegisterInput, register)
+router.post('/login', validateLoginInput, login)
 
 export default router
